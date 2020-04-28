4c67b77707b79ed6c045c489ff040935c72b46fd
2103962345 <alexbobylewa@gmail.com>
2020-04-28 01:57:18 -0700

    Update bat file

---
+++ build.ps1
@@ -0,0 +1,6 @@
+Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.PSVersion)"
+
+#Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { exit 1 }
+
+Write-Host "Congratulations! Your first script executed successfully"
+Start-Process -FilePath 'C:\Python27\python.exe' -ArgumentList 'C:\Users\Administrator\Downloads\python-package\hello_world.py'
---
+++ hello_world.py
@@ -0,0 +1,4 @@
+import time
+time.sleep(4)
+print("Hello World")
+time.sleep(2.4)
