63e802a7b82cbf9850b3eafa91d8669e92c49c55
2103962345 <alexbobylewa@gmail.com>
2020-04-28 02:02:32 -0700

    Update bat file

--- build.ps1
+++ build.ps1
@@ -3,4 +3,4 @@ Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.
 #Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { exit 1 }
 
 Write-Host "Congratulations! Your first script executed successfully"
-Start-Process -FilePath 'C:\Python27\python.exe' -ArgumentList 'C:\Users\Administrator\Downloads\python-package\hello_world.py'
+Start-Process -FilePath 'C:\Python38\python.exe' -ArgumentList 'C:\Users\Administrator\Downloads\python-package\hello_world.py'
