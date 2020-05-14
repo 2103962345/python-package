efc83e13c6dadfcab73dca8586a867ab7cff4ed2
2103962345 <2103962345@mail.ru>
2020-05-14 16:37:34 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -3,4 +3,4 @@ Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.
 #Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { exit 1 }
 
 Write-Host "Congratulations! Your first script executed successfully"
-Start-Process -FilePath python -ArgumentList '$PSScriptRoot\hello_world.py' -NoNewWindow
+Start-Process -FilePath python -ArgumentList "$PSScriptRoot\hello_world.py" -NoNewWindow
